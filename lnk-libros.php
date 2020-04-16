c29937b78be141dcaca475af0bb6830d831b2a4d
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2020-04-16 12:07:07 -0300

    file upload

--- lnk-libros.php
+++ lnk-libros.php
@@ -159,15 +159,13 @@ function lnk_libro_save_post_meta($id) {
         if (defined('DOING_AJAX') && DOING_AJAX)
                 return $id;
 
-        var_dump($_FILES);
-
         if(!empty($_FILES['lnk_libro_pdf']['name']) && $_FILES['lnk_libro_pdf']['error'] == 0) {
             $supported_types = array('application/pdf');
             $arr_file_type = wp_check_filetype(basename($_FILES['lnk_libro_pdf']['name']));
             $uploaded_type = $arr_file_type['type'];
 
             if(in_array($uploaded_type, $supported_types)) {
-                $upload = wp_upload_bits($_FILES['lnk_libro_pdf']['name'], null, file_get_contents($_FILES['lnk_libro_pdf']['name']));
+                $upload = wp_upload_bits($_FILES['lnk_libro_pdf']['name'], null, file_get_contents($_FILES['lnk_libro_pdf']['tmp_name']));
 
                 if(isset($upload['error']) && $upload['error'] != 0) {
                     wp_die('There was an error uploading your file. The error is: ' . $upload['error']);
